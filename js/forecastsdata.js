1b4d65b2b3be9c8f1b1cca813ec087d08992a5a3
rustamovmuhammadjon <mrustamov0109@gmail.com>
2024-12-24 16:48:36 +0500

    forecasts data
    forecasts va boshqa page lar uchun data ishlab chiqilmoqda websaytni yana dinamik usulda yaratish uchun

---
+++ js/forecastsdata.js
@@ -0,0 +1,110 @@
+const ForecastsData = [
+    {
+        id: 1,
+        image: '../images/aries.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 2,
+        image: '../images/taurus.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 3,
+        image: '../images/gemini.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 4,
+        image: '../images/cancer.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 5,
+        image: '../images/leo.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 6,
+        image: '../images/virgo.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 7,
+        image: '../images/libra.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 8,
+        image: '../images/scorpio.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 9,
+        image: '../images/sagittarius.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 10,
+        image: '../images/capricorn.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 11,
+        image: '../images/aquarius.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    },
+    {
+        id: 12,
+        image: '../images/pisces.svg',
+        name: '',
+        dayInfo: ['', ''],
+        weekInfo: ['', ''],
+        monthInfo: ['', ''],
+        yearInfo: ['', '']
+    }
+]
